ffefc14fcf331b26f71f60ac247711b5569d08e7
Arsalan Kashkoee <Arsalan.kashkoee@gmail.com>
2022-09-12 18:41:06 +0430

    my toggle

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import MyToggle from "./Components/MyToggle";
+
+function App() {
+  return (
+    <div className="container flex items-center justify-center">
+      <MyToggle />
+    </div>
+  );
+}
+
+export default App;
